f5d19288ee6b335ad70948bcd4f6d857ae238984
Sekiguchi Yuto <yutodazepa@icloud.com>
2020-11-11 13:10:56 +0900

    [add] MenuKit Component

--- app/components/HeaderMenu.tsx
+++ app/components/HeaderMenu.tsx
@@ -1,31 +1,18 @@
 import React, { useContext, useState } from "react";
-import Link from "next/link"
 
 import {
-    Avatar,
     createStyles,
-    Divider,
     IconButton,
-    ListItemIcon,
-    ListItemText,
     makeStyles,
-    Menu,
-    MenuItem,
-    MenuItemProps,
-    MenuProps,
-    withStyles
 } from "@material-ui/core"
 
 import {
     Skeleton
 } from '@material-ui/lab';
 
-import {
-    AccountCircle,
-    ExitToApp }
-from "@material-ui/icons";
-
 import { AuthContext } from "../contexts/auth";
+import { MenuKit } from "./UI/Menu";
+import { AvatarKit } from "./UI/Avatar";
 
 /**
  * Styles
@@ -39,78 +26,28 @@ const useStyles = makeStyles(() =>
     }),
 );
 
-const StyledMenu = withStyles({
-    paper: {
-        border: '1px solid #d3d3d3'
-    },
-}) ((props: MenuProps) => (
-    <Menu
-        elevation={0}
-        getContentAnchorEl={null}
-        anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
-        }}
-        transformOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
-        }}
-        {...props}
-    />
-))
-
-/**
- * Types
- */
-type LinkMenuItemProps = Omit<MenuItemProps<'a', { href: string }>, 'component' | 'button'>
-
-/**
- * Functions
- */
-const LinkMenuItem = React.forwardRef<HTMLAnchorElement, LinkMenuItemProps>(
-    function LinkMenuItem(props, forwardedRef) {
-
-        const {
-            href,
-            ...other
-        } = props
-
-        return (
-            <Link href={href}>
-                <MenuItem component='a' button ref={forwardedRef} {...other}/>
-            </Link>
-        )
-    }
-)
-
 export const HeaderMenu = () => {
     const classes = useStyles()
 
+    const { user, load, signout } = useContext(AuthContext)
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [open, setOpen] = useState(false)
 
     const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setOpen(true)
         setAnchorEl(event.currentTarget);
     };
 
     const handleClose = () => {
-        setOpen(false)
         setAnchorEl(null);
     };
 
-    // React
-    const { user, load, signout } = useContext(AuthContext)
-
-    const c = () => {
+    const handleLogout = () => {
         signout()
         setAnchorEl(null)
     }
 
     return (
         <div className={classes.toolbarButtons}>
-            {user ? (
+            {user && (
                 <div>
                     <IconButton
                         aria-controls="menu-appbar"
@@ -119,36 +56,18 @@ export const HeaderMenu = () => {
                         color="inherit"
                     >
                         {load ? (
-                            <Avatar>
-                                <Skeleton variant="circle" width={40} height={40} />
-                            </Avatar>
+                            <Skeleton variant="circle" width={40} height={40} />
                         ) : (
-                            <Avatar src={user.photoURL} alt="User Image" />
+                            <AvatarKit src={user.photoURL} />
                         )}
                     </IconButton>
-                    <StyledMenu
-                        id="menu-appbar"
+                    <MenuKit
                         anchorEl={anchorEl}
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                    >
-                        <LinkMenuItem href={`/users/${user.uid}`}>
-                                <ListItemIcon>
-                                    <AccountCircle fontSize="default" />
-                                </ListItemIcon>
-                                <ListItemText primary="マイページ" />
-                        </LinkMenuItem>
-                        <Divider />
-                        <MenuItem onClick={c}>
-                            <ListItemIcon>
-                                <ExitToApp fontSize="default" />
-                            </ListItemIcon>
-                            <ListItemText primary="ログアウト" />
-                        </MenuItem>
-                    </StyledMenu>
+                        uid={user.uid}
+                        handleClose={handleClose}
+                        handleLogout={handleLogout}
+                    />
                 </div>
-            ) : (
-                <div></div>
             )}
         </div>
     )
---
+++ app/components/UI/Menu.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import Link from "next/link"
+
+import {
+    Divider,
+    ListItemIcon,
+    ListItemText,
+    Menu,
+    MenuItem,
+    MenuItemProps,
+    MenuProps,
+    withStyles
+} from "@material-ui/core"
+
+import {
+    AccountCircle,
+    ExitToApp
+} from "@material-ui/icons"
+
+const StyledMenu = withStyles({
+    paper: {
+        border: '1px solid #d3d3d3'
+    },
+}) ((props: MenuProps) => (
+    <Menu
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+        }}
+        transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+        }}
+        {...props}
+    />
+))
+
+const LinkMenuItem = React.forwardRef<HTMLAnchorElement, LinkMenuItemProps>(
+    function LinkMenuItem(props, forwardedRef) {
+
+        const {
+            href,
+            ...other
+        } = props
+
+        return (
+            <Link href={href}>
+                <MenuItem component='a' button ref={forwardedRef} {...other}/>
+            </Link>
+        )
+    }
+)
+
+type LinkMenuItemProps = Omit<MenuItemProps<'a', { href: string }>, 'component' | 'button'>
+
+type MenuKitProps = {
+    anchorEl: null | HTMLElement
+    uid: string
+    handleClose: () => void
+    handleLogout: () => void
+}
+
+export const MenuKit = (props: MenuKitProps) => {
+    return (
+        <StyledMenu
+            id="menu-appbar"
+            anchorEl={props.anchorEl}
+            open={Boolean(props.anchorEl)}
+            onClose={props.handleClose}
+        >
+            <LinkMenuItem href={`/users/${props.uid}`}>
+                <ListItemIcon>
+                    <AccountCircle fontSize="default" />
+                </ListItemIcon>
+                <ListItemText primary="マイページ" />
+            </LinkMenuItem>
+            <Divider />
+            <MenuItem onClick={props.handleLogout}>
+                <ListItemIcon>
+                    <ExitToApp fontSize="default" />
+                </ListItemIcon>
+                <ListItemText primary="ログアウト" />
+            </MenuItem>
+        </StyledMenu>
+    )
+}
