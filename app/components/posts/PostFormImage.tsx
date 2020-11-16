bdaaa3af6bfacfc7e0e0c52b0008e9fa6f614123
Sekiguchi Yuto <yutodazepa@icloud.com>
2020-11-16 13:23:21 +0900

    [add] react-image-file-resizer

--- app/components/posts/PostFormImage.tsx
+++ app/components/posts/PostFormImage.tsx
@@ -1,113 +1,145 @@
-import { useCallback } from "react"
+import { useCallback } from 'react'
 
-import {
-    Chip,
-    createStyles,
-    makeStyles }
-from "@material-ui/core"
+import { Chip, createStyles, makeStyles } from '@material-ui/core'
 
-import {
-    PhotoCamera
-} from "@material-ui/icons"
+import { PhotoCamera } from '@material-ui/icons'
 
-import { PostFormImagePreview } from "./PostFormImagePreview"
-import { PreviewImage } from "../../models/Post"
+import { PostFormImagePreview } from './PostFormImagePreview'
+import { PreviewImage } from '../../models/Post'
+import Resizer from 'react-image-file-resizer'
 
 /** Styles */
 const useStyles = makeStyles(() =>
-    createStyles({
-        imageList: {
-            display: 'flex',
-            flexWrap: 'wrap',
-            width: '100%',
-        },
-        mt: {
-            marginTop: '8px',
-        },
-        input: {
-            display: 'none',
-        }
-    })
-);
+  createStyles({
+    imageList: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      width: '100%',
+    },
+    mt: {
+      marginTop: '8px',
+    },
+    input: {
+      display: 'none',
+    },
+  })
+)
 
 /** Types */
 type PostFormImageProps = {
-    previewImages: Array<PreviewImage>
-    setPreviewImages: Function
+  previewImages: Array<PreviewImage>
+  setPreviewImages: Function
 }
 
 /** Main */
 export const PostFormImage = (props: PostFormImageProps) => {
-    const classes = useStyles()
+  const classes = useStyles()
 
-    const upload = useCallback((e) => {
-        const files: Blob = e.target.files.item(0)
+  /**
+   * 画像をリサイズする
+   * @param file
+   */
+  const resizeFile = (file: File) =>
+    new Promise((resolve) => {
+      Resizer.imageFileResizer(
+        file,
+        500,
+        1000,
+        'JPEG',
+        100,
+        0,
+        (uri) => {
+          resolve(uri)
+        },
+        'blob'
+      )
+    })
 
-        //Blob作成
-        const file = e.target.files
-        const blob = new Blob(file, { type: "image/jpeg" })
+  /**
+   * 画像をプレビュー表示しPreviewImagesにsetする
+   */
+  const upload = useCallback(
+    async (e: React.ChangeEvent<HTMLInputElement>) => {
+      //fileName作成
+      const S = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+      const N = 16
+      const fileName = Array.from(crypto.getRandomValues(new Uint32Array(N)))
+        .map((n) => S[n % S.length])
+        .join('')
 
-        //fileName作成
-        const S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        const N = 16
-        const fileName = Array.from(crypto.getRandomValues(new Uint32Array(N))).map((n) => S[ n % S.length]).join('')
+      if (e.target.files) {
+        const file = e.target.files[0]
+        const blob = await resizeFile(file)
 
-        if (files) {
-            const reader = new FileReader()
-        
-            reader.onload = () => {
-                const previewImage = {
-                    id: fileName,
-                    path: reader.result,
-                    blob: blob,
-                }
-    
-                props.setPreviewImages(((prevState: Array<PreviewImage>) => [...prevState, previewImage]))
+        const reader = new FileReader()
+
+        if (file) {
+          reader.onload = () => {
+            const previewImage = {
+              id: fileName,
+              path: reader.result,
+              blob: blob,
             }
-            reader.readAsDataURL(files)
+
+            props.setPreviewImages((prevState: Array<PreviewImage>) => [
+              ...prevState,
+              previewImage,
+            ])
+          }
+          reader.readAsDataURL(file)
         }
-    }, [props.previewImages])
+      }
+    },
+    [props.previewImages]
+  )
 
-    const deleteImage = useCallback((id: string) => {
-        const newImages = props.previewImages.filter(image => image.id !== id)
+  /**
+   * PreviewImagesからidと一致するデータを取り除いた配列を作成
+   */
+  const deleteImage = useCallback(
+    (id: string) => {
+      const newImages = props.previewImages.filter((image) => image.id !== id)
 
-        props.setPreviewImages(newImages)
-    }, [props.previewImages])
+      props.setPreviewImages(newImages)
+    },
+    [props.previewImages]
+  )
 
-    return (
+  return (
+    <div>
+      <div className={classes.imageList}>
+        {props.previewImages.length > 0 &&
+          props.previewImages.map((image) => (
+            <>
+              <PostFormImagePreview
+                id={image.id}
+                path={image.path}
+                key={image.id}
+                delete={deleteImage}
+              />
+            </>
+          ))}
+      </div>
+
+      {props.previewImages.length < 4 && (
         <div>
-            <div className={classes.imageList}>
-                {props.previewImages.length > 0 && (
-                    props.previewImages.map(image =>
-                        <PostFormImagePreview
-                            id={image.id}
-                            path={image.path}
-                            key={image.id}
-                            delete={deleteImage}
-                        />
-                    )
-                )}
-            </div>
-
-            {props.previewImages.length < 4 && (
-                <div>
-                    <input
-                        accept="image/*"
-                        className={classes.input}
-                        id="icon-button-file"
-                        type="file"
-                        onChange={(e) => upload(e)}
-                    />
-                    <label htmlFor="icon-button-file">
-                        <Chip
-                            className={classes.mt}
-                            icon={<PhotoCamera />}
-                            label="画像を追加"
-                            variant="outlined"
-                        />
-                    </label>
-                </div>
-            )}
+          <input
+            accept="image/*"
+            className={classes.input}
+            id="icon-button-file"
+            type="file"
+            onChange={(e) => upload(e)}
+          />
+          <label htmlFor="icon-button-file">
+            <Chip
+              className={classes.mt}
+              icon={<PhotoCamera />}
+              label="画像を追加"
+              variant="outlined"
+            />
+          </label>
         </div>
-    )
+      )}
+    </div>
+  )
 }
--- app/pages/posts/create.tsx
+++ app/pages/posts/create.tsx
@@ -1,152 +1,138 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 import {
-    Button,
-    LinearProgress,
-    makeStyles,
-    TextField
-} from "@material-ui/core"
+  Button,
+  LinearProgress,
+  makeStyles,
+  TextField,
+} from '@material-ui/core'
 
-import { Image, PreviewImage } from "../../models/Post"
-import { PostFormImage } from "../../components/posts/PostFormImage"
-import { Layout } from "../../components/Layout"
-import { toast } from "react-toastify"
+import { Image, PreviewImage } from '../../models/Post'
+import { PostFormImage } from '../../components/posts/PostFormImage'
+import { Layout } from '../../components/Layout'
+import { toast } from 'react-toastify'
 import firebase from 'firebase/app'
 
 /** Styles */
 const useStyles = makeStyles(() => ({
-    progress: {
-        width: '100%'
-    },
-    container: {
-        margin: '0 auto',
-        marginTop: '20px',
-        maxWidth: '400px',
-        padding: '1rem',
-        height: 'auto',
-        width: 'calc(100% - 2rem)',
-    },
-    containerM: {
-        marginTop: '10px',
-        marginBottom: '10px',
-    }
+  progress: {
+    width: '100%',
+  },
+  container: {
+    margin: '0 auto',
+    marginTop: '20px',
+    maxWidth: '400px',
+    padding: '1rem',
+    height: 'auto',
+    width: 'calc(100% - 2rem)',
+  },
+  containerM: {
+    marginTop: '10px',
+    marginBottom: '10px',
+  },
 }))
 
 /** Main */
 const Create = () => {
-    const classes = useStyles();
+  const classes = useStyles()
 
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
-    const [previewImages, setPreviewImages] = useState<Array<PreviewImage>>([])
-    const [isSending, setIsSending] = useState(false)
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [previewImages, setPreviewImages] = useState<Array<PreviewImage>>([])
+  const [isSending, setIsSending] = useState(false)
 
-    const upload = async () => {
-        setIsSending(true)
+  const upload = async () => {
+    setIsSending(true)
 
-        const images: Array<Image> = []
+    const images: Array<Image> = []
 
-        await Promise.all(previewImages.map(async image => {
-            const uploadRef  = firebase.storage().ref('images').child(image.id)
-            const uploadTask = uploadRef.put(image.blob)
+    await Promise.all(
+      previewImages.map(async (image) => {
+        const uploadRef = firebase.storage().ref('images').child(image.id)
+        const uploadTask = uploadRef.put(image.blob)
 
-            const snapshot    = await uploadTask
-            const url: string = await snapshot.ref.getDownloadURL()
+        const snapshot = await uploadTask
+        const url: string = await snapshot.ref.getDownloadURL()
 
-            const newImage = {
-                id: image.id,
-                path: url
-            }
-            images.push(newImage)
-        }))
-
-        await firebase.firestore().collection('posts').add({
-            userId: firebase.auth().currentUser?.uid,
-            title,
-            images: images,
-            body,
-            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-        })
-
-        setPreviewImages([])
-        setIsSending(false)
-
-        toast.success('投稿しました。', {
-            position: 'bottom-left',
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-        })
-    }
-
-    return (
-        <Layout>
-            <div className={classes.container}>
-                <form>
-                    <TextField
-                        fullWidth={true}
-                        id="outlined-multiline-flexible"
-                        label="タイトル"
-                        multiline
-                        variant="outlined"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                    />
-                    <div className={classes.containerM}>
-                        <TextField
-                            fullWidth={true}
-                            id="outlined-multiline-static"
-                            label="本文"
-                            multiline
-                            rows={5}
-                            variant="outlined"
-                            value={body}
-                            onChange={(e) => setBody(e.target.value)}
-                        />
-                    </div>
-                    <PostFormImage
-                        previewImages={previewImages}
-                        setPreviewImages={setPreviewImages}
-                    />
-                    <div className={classes.containerM}>
-                        {isSending ? (
-                            <div className={classes.progress}>
-                                <LinearProgress />
-                            </div>
-                        ) : (
-                            <Button
-                                onClick={upload}
-                                fullWidth={true}
-                                variant="outlined"
-                                color="primary"
-                            >
-                                投稿
-                            </Button>
-                        )}
-                    </div>
-                </form>
-            </div>
-
-            <style jsx>{`
-                .container {
-                    margin: 0 auto;
-                    margin-top: 20px;
-                    max-width: 400px;
-                    padding: 1rem;
-                    height: auto;
-                    width: calc(100% - 2rem);
-                }
-
-                .container-m {
-                    margin-top: 10px;
-                    margin-bottom: 10px;
-                }
-            `}</style>
-        </Layout>
+        const newImage = {
+          id: image.id,
+          path: url,
+        }
+        images.push(newImage)
+      })
     )
+
+    await firebase.firestore().collection('posts').add({
+      userId: firebase.auth().currentUser?.uid,
+      title,
+      images: images,
+      body,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    })
+
+    setPreviewImages([])
+    setIsSending(false)
+
+    toast.success('投稿しました。', {
+      position: 'bottom-left',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    })
+  }
+
+  return (
+    <Layout>
+      <div className={classes.container}>
+        <form>
+          <TextField
+            fullWidth={true}
+            id="outlined-multiline-flexible"
+            label="タイトル"
+            multiline
+            variant="outlined"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <div className={classes.containerM}>
+            <TextField
+              fullWidth={true}
+              id="outlined-multiline-static"
+              label="本文"
+              multiline
+              rows={5}
+              variant="outlined"
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+            />
+          </div>
+          <PostFormImage
+            previewImages={previewImages}
+            setPreviewImages={setPreviewImages}
+          />
+          <div className={classes.containerM}>
+            {isSending ? (
+              <div className={classes.progress}>
+                <LinearProgress />
+              </div>
+            ) : (
+              <Button
+                onClick={upload}
+                fullWidth={true}
+                variant="outlined"
+                color="primary"
+              >
+                投稿
+              </Button>
+            )}
+          </div>
+        </form>
+      </div>
+    </Layout>
+  )
 }
 
 export default Create
