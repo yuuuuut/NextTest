42376c4e4ec4be055cc0503ab145d66c8d264b40
Sekiguchi Yuto <yutodazepa@icloud.com>
2020-11-27 16:39:17 +0900

    [add] PostCardMediaPreview

--- app/components/posts/PostCardMediaChild.tsx
+++ app/components/posts/PostCardMediaChild.tsx
@@ -1,20 +1,12 @@
 import { useState } from 'react'
 
-import {
-  Backdrop,
-  Box,
-  createStyles,
-  IconButton,
-  makeStyles,
-  Theme,
-} from '@material-ui/core'
+import { createStyles, makeStyles } from '@material-ui/core'
 
-import { ChevronLeft, ChevronRight, Close } from '@material-ui/icons'
-
+import { PostCardMediaPreview } from './PostCardMediaPreview'
 import { Image } from '../../models/Post'
 
 /** Style */
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
     twoImage: {
       width: 150,
@@ -33,30 +25,6 @@ const useStyles = makeStyles((theme: Theme) =>
       height: 85,
       objectFit: 'cover',
       margin: 0.5,
-    },
-    backdrop: {
-      zIndex: theme.zIndex.drawer + 1,
-      backgroundColor: 'rgba(0, 0, 0, 0.4)',
-      color: '#fff',
-    },
-    backdropImage: {
-      width: '100%',
-      maxWidth: '100%',
-      height: 'auto',
-    },
-    iconButtonBg: {
-      backgroundColor: '#777777',
-      opacity: 0.9,
-      pointerEvents: 'none',
-    },
-    imageCount: {
-      position: 'fixed',
-      top: '50&',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-    },
-    white: {
-      color: 'white',
     },
   })
 )
@@ -83,14 +51,6 @@ export const PostCardMediaChild = (props: PostCardMediaChildProps) => {
   const [open, setOpen] = useState(false)
 
   /**
-   * styleAttributeを削除してopenをfalseに
-   */
-  const handleClose = () => {
-    document.body.removeAttribute('style')
-    setOpen(false)
-  }
-
-  /**
    * styleAttributeを設定して、currentIndex・currentImageをセット、
    * openを反転させる
    * @param idx
@@ -101,6 +61,14 @@ export const PostCardMediaChild = (props: PostCardMediaChildProps) => {
     setCurrentIndex(idx)
     setCurrentImage(images[idx].path)
     setOpen(!open)
+  }
+
+  /**
+   * styleAttributeを削除してopenをfalseに
+   */
+  const handleClose = () => {
+    document.body.removeAttribute('style')
+    setOpen(false)
   }
 
   /**
@@ -137,37 +105,15 @@ export const PostCardMediaChild = (props: PostCardMediaChildProps) => {
             : classes.fourImage
         }
       />
-      <Backdrop className={classes.backdrop} open={open}>
-        <Box position="absolute" top={20} right={30} onClick={handleClose}>
-          <IconButton className={classes.iconButtonBg}>
-            <Close className={classes.white} fontSize="large" />
-          </IconButton>
-        </Box>
-        <div>
-          <img src={currentImage} className={classes.backdropImage} />
-          <h3 className={classes.imageCount}>
-            {currentIndex + 1 + '/' + imgLength}
-          </h3>
-        </div>
-        <Box position="absolute" top={325} right={20}>
-          {currentIndex !== imgLength - 1 && (
-            <div onClick={() => add(currentIndex)}>
-              <IconButton color="primary" className={classes.iconButtonBg}>
-                <ChevronRight className={classes.white} fontSize="large" />
-              </IconButton>
-            </div>
-          )}
-        </Box>
-        <Box position="absolute" top={325} left={20}>
-          {currentIndex !== 0 && (
-            <div onClick={() => back(currentIndex)}>
-              <IconButton color="primary" className={classes.iconButtonBg}>
-                <ChevronLeft className={classes.white} fontSize="large" />
-              </IconButton>
-            </div>
-          )}
-        </Box>
-      </Backdrop>
+      <PostCardMediaPreview
+        open={open}
+        imgLength={imgLength}
+        currentImage={currentImage}
+        currentIndex={currentIndex}
+        handleClose={handleClose}
+        add={add}
+        back={back}
+      />
     </>
   )
 }
---
+++ app/components/posts/PostCardMediaPreview.tsx
@@ -0,0 +1,85 @@
+import {
+  Backdrop,
+  Box,
+  createStyles,
+  IconButton,
+  makeStyles,
+  Theme,
+} from '@material-ui/core'
+import { ChevronLeft, ChevronRight, Close } from '@material-ui/icons'
+
+/** Style */
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      backgroundColor: 'rgba(0, 0, 0, 0.4)',
+      color: '#fff',
+    },
+    backdropImage: {
+      width: '100%',
+      maxWidth: '100%',
+      height: 'auto',
+    },
+    iconButtonBg: {
+      backgroundColor: '#777777',
+      opacity: 0.9,
+      pointerEvents: 'none',
+    },
+    white: {
+      color: 'white',
+    },
+  })
+)
+
+/** Types */
+type PostCardMediaPreviewProps = {
+  imgLength: number
+  open: boolean
+  currentImage: string
+  currentIndex: number
+  handleClose: () => void
+  add: (id: number) => void
+  back: (id: number) => void
+}
+
+/** Main */
+export const PostCardMediaPreview = (props: PostCardMediaPreviewProps) => {
+  const classes = useStyles()
+
+  const open = props.open
+  const imgLength = props.imgLength
+  const currentImage = props.currentImage
+  const currentIndex = props.currentIndex
+
+  return (
+    <Backdrop className={classes.backdrop} open={open}>
+      <Box position="absolute" top={20} right={30} onClick={props.handleClose}>
+        <IconButton className={classes.iconButtonBg}>
+          <Close className={classes.white} fontSize="large" />
+        </IconButton>
+      </Box>
+      <div>
+        <img src={currentImage} className={classes.backdropImage} />
+      </div>
+      <Box position="absolute" top={325} right={20}>
+        {currentIndex !== imgLength - 1 && (
+          <div onClick={() => props.add(currentIndex)}>
+            <IconButton color="primary" className={classes.iconButtonBg}>
+              <ChevronRight className={classes.white} fontSize="large" />
+            </IconButton>
+          </div>
+        )}
+      </Box>
+      <Box position="absolute" top={325} left={20}>
+        {currentIndex !== 0 && (
+          <div onClick={() => props.back(currentIndex)}>
+            <IconButton color="primary" className={classes.iconButtonBg}>
+              <ChevronLeft className={classes.white} fontSize="large" />
+            </IconButton>
+          </div>
+        )}
+      </Box>
+    </Backdrop>
+  )
+}
